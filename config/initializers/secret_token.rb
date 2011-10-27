3f29e1d5a6d27390d120e90e6632b08486a2a488
Jean Bahnik <jeanbahnik@gmail.com>
2011-10-27 12:58:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beebuzz::Application.config.secret_token = '5118287bc05fdd8ce56db4dda92408fbc822bd47891d111d1e86fe0eb82af108f160a2e667642da80b7b335346e67485101ed1877b60a7aecf54216bbe5a4bca'
